0b9317ef48c04e8330f89513e8c89d089891c711
Guanghui Yu <earayu@163.com>
2017-11-26 23:45:05 +0800

    init
    后台初始化

---
+++ ncufc-backend/src/main/java/cn/eovie/ncufcbackend/NcufcBackendApplication.java
@@ -0,0 +1,12 @@
+package cn.eovie.ncufcbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NcufcBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NcufcBackendApplication.class, args);
+	}
+}
